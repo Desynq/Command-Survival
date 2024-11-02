f70cf27ea3e096dadf0ba35ed1edd30c9c6e7498
Desynq <notdesynq@gmail.com>
2024-11-02 03:37:05 -0400

    Hopefully fixed quartz dimdoors not being indestructible

--- src/startup.ts
+++ src/startup.ts
@@ -1,5 +1,5 @@
 BlockEvents.modification(event => {
-	event.modify("dimdoors:item_ag_dim_dimdoors_quartz_door" as any, block => {
+	event.modify("dimdoors:block_ag_dim_dimdoors_quartz_door" as any, block => {
 		block.destroySpeed = -1;
 		block.explosionResistance = 3600000;
 	});
