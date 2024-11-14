2a582a8bf31ab2790f210fe0d71d694b3479ce09
Desynq <notdesynq@gmail.com>
2024-11-14 02:50:53 -0500

    Fixed playsound and particle of sculker absorb ability and changed error messages

--- src/server/commands/run/SculkerAbsorb.ts
+++ src/server/commands/run/SculkerAbsorb.ts
@@ -17,31 +17,35 @@ export class SculkerAbsorb {
 
 	private attemptAdd(): void {
 		if (this.absorbLevel >= 5) {
-			this.player.tell(Component.red("You're at maximum experience absorption-- cannot upgrade any further.") as any);
+			this.player.statusMessage = "You're at maximum experience absorption. Cannot upgrade any further." as any;
 			return;
 		}
 		if (this.player.xp < this.COST) {
-			this.player.tell(Component.red(`Cannot afford to upgrade-- missing ${this.COST - this.player.xp} xp.`) as any);
+			this.player.statusMessage = `Cannot afford to upgrade. Missing ${this.COST - this.player.xp} xp.` as any;
 			return;
 		}
-
-		SculkOrigin.addAbsorbLevel(this.player, 1);
-		this.player.xp -= this.COST;
-		this.player.playSound("minecraft:block.sculk_shrieker.shriek", 1, 1);
-		Utils.server.runCommandSilent(`execute at ${this.player.username} run particle "minecraft:sculk_soul" ~ ~1 ~ 0.5 0.7 0.5 0.1 50 force @a`);
-		new ApplyAttributesManager(this.player);
+		this.endResult(true);
 	}
 
 	private attemptRemove(): void {
-		if (this.absorbLevel < 0) {
-			this.player.tell(Component.red("Cannot unabsorb experience-- no experience stored as absorption levels.") as any);
+		if (this.absorbLevel <= 0) {
+			this.player.statusMessage = Component.red("Cannot unabsorb experience. No experience stored as absorption levels.") as any;
 			return;
 		}
+		this.endResult(false);
+	}
 
-		SculkOrigin.addAbsorbLevel(this.player, -1);
-		this.player.xp += this.COST;
-		this.player.playSound("minecraft:block.sculk_sensor.clicking", 1, 1);
-		Utils.server.runCommandSilent(`execute at ${this.player.username} run particle "minecraft:ash" ~ ~1 ~ 0.5 0.7 0.5 0.1 50 force @a`);
+	private endResult(adding: boolean): void {
+		const modifier = adding ? 1 : -1;
+		SculkOrigin.addAbsorbLevel(this.player, modifier);
+		this.player.xp += this.COST * -modifier;
+
+		const soundEvent = adding ? "minecraft:block.sculk_shrieker.shriek" : "minecraft:block.sculk_sensor.clicking";
+		this.player.level.playSound(null as any, this.player.blockX, this.player.blockY, this.player.blockZ, soundEvent, "master", 1, 1);
+
+		const particle = adding ? "minecraft:sculk_soul" : "minecraft:ash";
+		Utils.server.runCommandSilent(`execute at ${this.player.username} run particle ${particle} ~ ~1 ~ 0.5 0.7 0.5 0.1 50 force @a`);
+
 		new ApplyAttributesManager(this.player);
 	}
 
