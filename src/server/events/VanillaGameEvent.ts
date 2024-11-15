f274e2161cba72ec930fcb2227b16dee02b4a38a
Desynq <notdesynq@gmail.com>
2024-11-15 01:43:42 -0500

    Sculker pinging now ignores hitting the ground while crouching and is less effective against guns with a mirage suppressor

--- src/server/events/VanillaGameEvent.ts
+++ src/server/events/VanillaGameEvent.ts
@@ -16,11 +16,11 @@ function createAABB(x: number, y: number, z: number, distance: number): Internal
 
 
 global.events.VanillaGameEvent = function (event: Internal.VanillaGameEvent) {
-	const { cause: sourceEntity, level, vanillaEvent } = event;
+	const { cause: sourceEntity, level, vanillaEvent: gameEvent } = event;
 	if (!(sourceEntity instanceof $LivingEntity)) {
 		return;
 	}
-	if (sourceEntity.crouching && vanillaEvent.name === "step") {
+	if (sourceEntity.crouching && ["step", "hit_ground"].indexOf(gameEvent.name) !== -1) {
 		return;
 	}
 
@@ -32,8 +32,29 @@ global.events.VanillaGameEvent = function (event: Internal.VanillaGameEvent) {
 		if (player.distanceToEntity(sourceEntity as any) > 64) {
 			continue;
 		}
+		if (gameEvent.name === "projectile_shoot") {
+			const distance = projectileShootGameEvent(sourceEntity, level, gameEvent);
+			if (player.distanceToEntity(sourceEntity as any) > distance) continue;
+		}
 		
+		// sourceEntity.statusMessage = `${gameEvent.name}` as any;
 		sourceEntity.addEffect(new $MobEffectInstance("kubejs:pinged", 20, 0, false, false, true));
 		break;
 	}
+}
+
+function projectileShootGameEvent(sourceEntity: Internal.LivingEntity, level: Internal.Level, gameEvent: Internal.GameEvent): number {
+	const DEFAULT_DISTANCE = 64;
+	const itemStack = sourceEntity.mainHandItem;
+	if (itemStack == null) return DEFAULT_DISTANCE;
+	if (itemStack.id !== "tacz:modern_kinetic_gun") return DEFAULT_DISTANCE;
+	
+	const muzzleAttachment = itemStack?.nbt?.getCompound("AttachmentMUZZLE")?.getCompound("tag")?.getString("AttachmentId");
+	if (muzzleAttachment == null) return DEFAULT_DISTANCE;
+
+	switch (muzzleAttachment) {
+		case "tacz:muzzle_silence_mirage":
+			return 8;
+	}
+	return DEFAULT_DISTANCE;
 }
