d8766ba24bf40ba29a7b4b555bdf5ed761eca03a
Desynq <notdesynq@gmail.com>
2024-11-24 06:44:54 -0500

    Debug.logAdmin now displays logged message as a painter element

--- src/libraries/Debug.ts
+++ src/libraries/Debug.ts
@@ -2,15 +2,36 @@
 
 
 export class Debug {
-	public static logAdmin(message: string) {
-		const players: Internal.Player[] = Utils.server.players.toArray();
-		if (!players) {
+	/**
+	 * @deprecated
+	 */
+	public static logAdmin(message: string): void {
+		this.logToAdmins(message);
+	}
+
+	public static logToAdmins(message: string): void {
+		for (let player of Utils.server.players as unknown as Player[]) {
+			this.logIfAdmin(player, message);
+		}
+	}
+
+	public static logIfAdmin(player: Player, message: string): void {
+		if (!player.hasPermissions(4)) {
 			return;
 		}
-		const admins = players.filter(player => player.hasPermissions(4));
-		if (!admins) {
-			return;
-		}
-		admins.forEach(admin => admin.tell(message as any));
+
+		player.paint({
+			"debug": {
+				type: "text",
+				text: `${message}`,
+				color: "white",
+				shadow: true,
+				alignX: "left",
+				alignY: "bottom",
+				x: 0,
+				y: 0,
+				scale: 0.5
+			}
+		});
 	}
 }
